5c30e677abd584caf2604c0e549bef504d0b6702
bhagwat7540 <82805244+bhagwat7540@users.noreply.github.com>
2022-08-01 16:31:44 +0530

    Time: 549 ms (65.99%), Space: 10.3 MB (52.64%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -23,8 +23,26 @@ public:
         nums.insert(nums.begin() , 1);
         int n = nums.size();
         
-        vector<vector<int>> dp(n , vector<int>(n , -1));
+        vector<vector<int>> dp(n , vector<int>(n , 0));
         
-        return f(1 , n-2 , nums , dp);
+        for(int i=n-2 ; i>=1 ; i--){
+            for(int j=1 ; j<=n-2 ; j++){
+                if(i>j) continue;
+                
+                int maxx = 0;
+                
+                for(int ind=i ; ind<=j ; ind++){
+                    int ans = nums[i-1]*nums[ind]*nums[j+1];
+                    ans += dp[i][ind-1] + dp[ind+1][j];
+                    
+                    maxx = max(maxx , ans);
+                }
+                
+                dp[i][j] = maxx;
+            }
+        }
+        
+        return dp[1][n-2];
+        // return f(1 , n-2 , nums , dp);
     }
 };
