5966d425500d19e50303dada7adb938839e5c66a
bhagwat7540 <82805244+bhagwat7540@users.noreply.github.com>
2022-08-06 03:32:40 +0530

    Time: 3 ms (99.09%), Space: 11.1 MB (88.20%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,22 +17,55 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        map<Node* , Node*> mp;
+//         map<Node* , Node*> mp;
+        
+//         Node* ptr = head;
+//         while(ptr){
+//             mp[ptr] = new Node(ptr->val);
+//             ptr = ptr->next;
+//         }
+        
+//         ptr = head;
+        
+//         while(ptr){
+//             mp[ptr]->next = mp[ptr->next];
+//             mp[ptr]->random = mp[ptr->random];
+//             ptr = ptr->next;
+//         }
+        
+//         return mp[head];
         
         Node* ptr = head;
+        
         while(ptr){
-            mp[ptr] = new Node(ptr->val);
-            ptr = ptr->next;
+            Node* front = ptr->next;
+            Node* copy = new Node(ptr->val);
+            ptr->next = copy;
+            copy->next = front;
+            ptr = front;
         }
         
         ptr = head;
-        
         while(ptr){
-            mp[ptr]->next = mp[ptr->next];
-            mp[ptr]->random = mp[ptr->random];
-            ptr = ptr->next;
+            if(ptr->random){
+               ptr->next->random = ptr->random->next;
+            }
+            ptr = ptr->next->next; 
         }
         
-        return mp[head];
+        Node* dummy = new Node(0);
+        Node* temp = dummy;
+        ptr = head;
+        
+        while(ptr){
+            Node* front = ptr->next->next;
+            temp->next = ptr->next;
+            ptr->next = front;
+            ptr = front;
+            temp = temp->next;
+        }
+        
+        return dummy->next;
+        
     }
 };
