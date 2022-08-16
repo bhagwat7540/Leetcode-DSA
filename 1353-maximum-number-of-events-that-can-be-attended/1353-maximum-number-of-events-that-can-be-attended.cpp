af28247062fdf222b554f547d3aebaf4cc0049f2
bhagwat7540 <82805244+bhagwat7540@users.noreply.github.com>
2022-08-16 13:32:51 +0530

    Time: 532 ms (69.49%), Space: 70.7 MB (92.65%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     
-    static bool comp(vector<int>& a , vector<int>& b){
-        if(a[1]<b[1]) return true;
-        if(a[1]==b[1] && a[0]<=b[0]) return true;
-        return false;
-    }
-    
-    int maxEvents(vector<vector<int>>& events) {
+  int maxEvents(vector<vector<int>>& events) {
         sort(events.begin() , events.end());
         priority_queue<int , vector<int> , greater<int>> pq;
         int n = events.size();
         int j=0 , ans=0;
         
-        for(int i=1 ; i<=1e5 ; i++){
+        for(int i=0 ; i<=1e5 ; i++){
+            while(!pq.empty() && pq.top() < i) pq.pop();
             
-            while(!pq.empty() && pq.top()<i) pq.pop();
+            while(j<n && events[j][0] == i) pq.push(events[j][1]) , j++;
             
-            while(j<n && events[j][0] == i){
-                pq.push(events[j][1]);
-                j++;
-            }
-            
-            if(!pq.empty()){
-                pq.pop();
-                ans++;
-            }
+            if(!pq.empty()) ans++ , pq.pop();
             
             if(j==n && pq.empty()) break;
         }
         
         return ans;
-        
     }
 };
