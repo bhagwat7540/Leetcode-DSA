fcf4e1cb02c3c3c68e5578dd18395348a773f19f
bhagwat7540 <82805244+bhagwat7540@users.noreply.github.com>
2022-08-18 23:33:13 +0530

    Time: 3 ms (82.45%), Space: 7.8 MB (54.57%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     set<vector<int>> s;
+    vector<vector<int>> ans;
     
     void f(int i , vector<int>& a , vector<int>& v){
         
-        if(i == a.size()){
-            s.insert(v);
-            return;
+        ans.push_back(v);
+        
+        for(int j=i ; j<a.size() ; j++){
+            if(j == i || a[j] != a[j-1]){
+                v.push_back(a[j]);
+                f(j+1 , a , v);
+                v.pop_back();
+            }
         }
         
-        f(i+1 , a , v);
-        v.push_back(a[i]);
-        f(i+1 , a , v);
-        v.pop_back();
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
@@ -21,10 +23,10 @@ public:
         vector<int> v;
         f(0 , nums , v);
         
-        vector<vector<int>> ans;
-        for(auto x : s){
-            ans.push_back(x);
-        }
+        // vector<vector<int>> ans;
+        // for(auto x : s){
+        //     ans.push_back(x);
+        // }
         
         return ans;
     }
